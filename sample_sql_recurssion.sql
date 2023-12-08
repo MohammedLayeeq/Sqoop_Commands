c5287d17f97af3acc1499c830768aa3f4c5789c4
Mohammed Layeeq <36068294+MohammedLayeeq@users.noreply.github.com>
2023-12-08 09:26:44 +0530

    Add files via upload

---
+++ sample_sql_recurssion.sql
@@ -0,0 +1,410 @@
+CREATE TABLE sample_layeeq (
+    location_no INT,
+    location_name VARCHAR(50),
+    ucid INT,
+    item_tran_selling DECIMAL(10, 2),
+    item_discount_selling DECIMAL(10, 2),
+    item_tran_qty INT,
+    tran_ind INT,
+    basket_id INT,
+    fin_year_month_no INT
+);
+
+
+
+
+-- Insert sample data
+INSERT INTO sample_layeeq VALUES
+    (1, 'Location A', 101, 25.0, 2.5, 3, 1, 4, 202404),
+    (1, 'Location A', 102, 30.0, 3.0, 2, -1, 5, 202404),
+    (2, 'Location B', 103, 35.0, 3.5, 4, 1, 6, 202404),
+    (2, 'Location B', 104, 40.0, 4.0, 1, -1, 7, 202404),
+    (3, 'Location C', 105, 45.0, 4.5, 2, 1, 8, 202404),
+    (3, 'Location C', 106, 50.0, 5.0, 3, -1, 9, 202404),
+    -- Add more sample data as needed
+    (1, 'Location A', 107, 55.0, 5.5, 1, 1, 10, 202404),
+    (2, 'Location B', 108, 60.0, 6.0, 2, -1, 11, 202404),
+    (3, 'Location C', 109, 65.0, 6.5, 3, 1, 12, 202404);
+
+-- Additional sample data for other locations
+INSERT INTO sample_layeeq VALUES
+    (4, 'Location D', 110, 70.0, 7.0, 4, -1, 13, 202404),
+    (4, 'Location D', 111, 75.0, 7.5, 1, 1, 14, 202404),
+    (5, 'Location E', 112, 80.0, 8.0, 2, -1, 15, 202404),
+    (5, 'Location E', 113, 85.0, 8.5, 3, 1, 16, 202404),
+    -- Add more sample data for other locations as needed
+    (6, 'Location F', 114, 90.0, 9.0, 4, -1, 17, 202404),
+    (6, 'Location F', 115, 95.0, 9.5, 1, 1, 18, 202404);
+	
+	
+	INSERT INTO sample_layeeq VALUES
+    (1, 'Location A', 101, 25.0, 2.5, 3, 1, 4, 202404),
+    (1, 'Location A', 102, 30.0, 3.0, 2, -1, 5, 202404),
+    (2, 'Location B', 103, 35.0, 3.5, 4, 1, 6, 202404),
+    (2, 'Location B', 104, 40.0, 4.0, 1, -1, 7, 202404),
+    (3, 'Location C', 105, 45.0, 4.5, 2, 1, 8, 202404),
+    (3, 'Location C', 106, 50.0, 5.0, 3, -1, 9, 202404),
+    -- Add more sample data as needed
+    (1, 'Location A', 107, 55.0, 5.5, 1, 1, 10, 202404),
+    (2, 'Location B', 108, 60.0, 6.0, 2, -1, 11, 202404),
+    (3, 'Location C', 109, 65.0, 6.5, 3, 1, 12, 202404);
+
+-- Additional sample data for other locations
+INSERT INTO sample_layeeq VALUES
+    (4, 'Location D', 110, 70.0, 7.0, 4, -1, 13, 202404),
+    (4, 'Location D', 111, 75.0, 7.5, 1, 1, 14, 202404),
+    (5, 'Location E', 112, 80.0, 8.0, 2, -1, 15, 202404),
+    (5, 'Location E', 113, 85.0, 8.5, 3, 1, 16, 202404),
+    -- Add more sample data for other locations as needed
+    (6, 'Location F', 114, 90.0, 9.0, 4, -1, 17, 202404),
+    (6, 'Location F', 115, 95.0, 9.5, 1, 1, 18, 202404);
+	
+		INSERT INTO sample_layeeq VALUES
+    (1, 'Location A', 101, 25.0, 2.5, 3, 1, 4, 202404),
+    (1, 'Location A', 102, 30.0, 3.0, 2, -1, 5, 202404),
+    (2, 'Location B', 103, 35.0, 3.5, 4, 1, 6, 202404),
+    (2, 'Location B', 104, 40.0, 4.0, 1, -1, 7, 202404),
+    (3, 'Location C', 105, 45.0, 4.5, 2, 1, 8, 202404),
+    (3, 'Location C', 106, 50.0, 5.0, 3, -1, 9, 202404),
+    -- Add more sample data as needed
+    (1, 'Location A', 107, 55.0, 5.5, 1, 1, 10, 202404),
+    (2, 'Location B', 108, 60.0, 6.0, 2, -1, 11, 202404),
+    (3, 'Location C', 109, 65.0, 6.5, 3, 1, 12, 202404);
+
+-- Additional sample data for other locations
+INSERT INTO sample_layeeq VALUES
+    (4, 'Location D', 110, 70.0, 7.0, 4, -1, 13, 202404),
+    (4, 'Location D', 111, 75.0, 7.5, 1, 1, 14, 202404),
+    (5, 'Location E', 112, 80.0, 8.0, 2, -1, 15, 202404),
+    (5, 'Location E', 113, 85.0, 8.5, 3, 1, 16, 202404),
+    -- Add more sample data for other locations as needed
+    (6, 'Location F', 114, 90.0, 9.0, 4, -1, 17, 202404),
+    (6, 'Location F', 115, 95.0, 9.5, 1, 1, 18, 202404);
+	
+		INSERT INTO sample_layeeq VALUES
+    (1, 'Location A', 101, 25.0, 2.5, 3, 1, 4, 202404),
+    (1, 'Location A', 102, 30.0, 3.0, 2, -1, 5, 202404),
+    (2, 'Location B', 103, 35.0, 3.5, 4, 1, 6, 202404),
+    (2, 'Location B', 104, 40.0, 4.0, 1, -1, 7, 202404),
+    (3, 'Location C', 105, 45.0, 4.5, 2, 1, 8, 202404),
+    (3, 'Location C', 106, 50.0, 5.0, 3, -1, 9, 202404),
+    -- Add more sample data as needed
+    (1, 'Location A', 107, 55.0, 5.5, 1, 1, 10, 202404),
+    (2, 'Location B', 108, 60.0, 6.0, 2, -1, 11, 202404),
+    (3, 'Location C', 109, 65.0, 6.5, 3, 1, 12, 202404);
+
+-- Additional sample data for other locations
+INSERT INTO sample_layeeq VALUES
+    (4, 'Location D', 110, 70.0, 7.0, 4, -1, 13, 202404),
+    (4, 'Location D', 111, 75.0, 7.5, 1, 1, 14, 202404),
+    (5, 'Location E', 112, 80.0, 8.0, 2, -1, 15, 202404),
+    (5, 'Location E', 113, 85.0, 8.5, 3, 1, 16, 202404),
+    -- Add more sample data for other locations as needed
+    (6, 'Location F', 114, 90.0, 9.0, 4, -1, 17, 202404),
+    (6, 'Location F', 115, 95.0, 9.5, 1, 1, 18, 202404);
+	
+		INSERT INTO sample_layeeq VALUES
+    (1, 'Location A', 101, 25.0, 2.5, 3, 1, 4, 202404),
+    (1, 'Location A', 102, 30.0, 3.0, 2, -1, 5, 202404),
+    (2, 'Location B', 103, 35.0, 3.5, 4, 1, 6, 202404),
+    (2, 'Location B', 104, 40.0, 4.0, 1, -1, 7, 202404),
+    (3, 'Location C', 105, 45.0, 4.5, 2, 1, 8, 202404),
+    (3, 'Location C', 106, 50.0, 5.0, 3, -1, 9, 202404),
+    -- Add more sample data as needed
+    (1, 'Location A', 107, 55.0, 5.5, 1, 1, 10, 202404),
+    (2, 'Location B', 108, 60.0, 6.0, 2, -1, 11, 202404),
+    (3, 'Location C', 109, 65.0, 6.5, 3, 1, 12, 202404);
+
+-- Additional sample data for other locations
+INSERT INTO sample_layeeq VALUES
+    (4, 'Location D', 110, 70.0, 7.0, 4, -1, 13, 202404),
+    (4, 'Location D', 111, 75.0, 7.5, 1, 1, 14, 202404),
+    (5, 'Location E', 112, 80.0, 8.0, 2, -1, 15, 202404),
+    (5, 'Location E', 113, 85.0, 8.5, 3, 1, 16, 202404),
+    -- Add more sample data for other locations as needed
+    (6, 'Location F', 114, 90.0, 9.0, 4, -1, 17, 202404),
+    (6, 'Location F', 115, 95.0, 9.5, 1, 1, 18, 202404);
+	
+	
+	----
+	
+	
+		INSERT INTO sample_layeeq VALUES
+    (9, 'Location A9', 101, 25.0, 2.5, 3, 1, 4, 202404),
+    (9, 'Location A9', 102, 30.0, 3.0, 2, -1, 5, 202404),
+    (9, 'Location A9', 103, 35.0, 3.5, 4, 1, 6, 202404),
+    (9, 'Location A9', 104, 40.0, 4.0, 1, -1, 7, 202404),
+    (9, 'Location A9', 105, 45.0, 4.5, 2, 1, 8, 202404),
+    (9, 'Location A9', 106, 50.0, 5.0, 3, -1, 9, 202404),
+    -- Add more sample data as needed
+    (9, 'Location A9', 107, 55.0, 5.5, 1, 1, 10, 202404),
+    (9, 'Location A9', 108, 60.0, 6.0, 2, -1, 11, 202404),
+    (9, 'Location A9', 109, 65.0, 6.5, 3, 1, 12, 202404);
+
+-- Additional sample data for other locations
+INSERT INTO sample_layeeq VALUES
+    (9, 'Location A9', 110, 70.0, 7.0, 4, -1, 13, 202404),
+    (9, 'Location A9', 111, 75.0, 7.5, 1, 1, 14, 202404),
+    (9, 'Location A9', 112, 80.0, 8.0, 2, -1, 15, 202404),
+    (9, 'Location A9', 113, 85.0, 8.5, 3, 1, 16, 202404),
+    -- Add more sample data for other locations as needed
+    (9, 'Location A9', 114, 90.0, 9.0, 4, -1, 17, 202404),
+    (9, 'Location A9', 115, 95.0, 9.5, 1, 1, 18, 202404);
+	
+	
+	
+		INSERT INTO sample_layeeq VALUES
+    (7, 'Location A7', 101, 25.0, 2.5, 3, 1, 4, 202404),
+    (7, 'Location A7', 102, 30.0, 3.0, 2, -1, 5, 202404),
+    (7, 'Location A7', 103, 35.0, 3.5, 4, 1, 6, 202404),
+    (7, 'Location A7', 104, 40.0, 4.0, 1, -1, 7, 202404),
+    (7, 'Location A7', 105, 45.0, 4.5, 2, 1, 8, 202404),
+    (7, 'Location A7', 106, 50.0, 5.0, 3, -1, 9, 202404),
+    -- Add more sample data as needed
+    (7, 'Location A7', 107, 55.0, 5.5, 1, 1, 10, 202404),
+    (7, 'Location A7', 108, 60.0, 6.0, 2, -1, 11, 202404),
+    (7, 'Location A7', 109, 65.0, 6.5, 3, 1, 12, 202404);
+
+-- Additional sample data for other locations
+INSERT INTO sample_layeeq VALUES
+    (7, 'Location A7', 110, 70.0, 7.0, 4, -1, 13, 202404),
+    (7, 'Location A7', 111, 75.0, 7.5, 1, 1, 14, 202404),
+    (7, 'Location A7', 112, 80.0, 8.0, 2, -1, 15, 202404),
+    (7, 'Location A7', 113, 85.0, 8.5, 3, 1, 16, 202404),
+    -- Add more sample data for other locations as needed
+    (7, 'Location A7', 114, 90.0, 9.0, 4, -1, 17, 202404),
+    (7, 'Location A7', 115, 95.0, 9.5, 1, 1, 18, 202404);
+	
+	
+		INSERT INTO sample_layeeq VALUES
+    (7, 'Location A7', 101, 25.0, 2.5, 3, 1, 4, 202404),
+    (7, 'Location A7', 102, 30.0, 3.0, 2, -1, 5, 202404),
+    (7, 'Location A7', 103, 35.0, 3.5, 4, 1, 6, 202404),
+    (7, 'Location A7', 104, 40.0, 4.0, 1, -1, 7, 202404),
+    (7, 'Location A7', 105, 45.0, 4.5, 2, 1, 8, 202404),
+    (7, 'Location A7', 106, 50.0, 5.0, 3, -1, 9, 202404),
+    -- Add more sample data as needed
+    (7, 'Location A7', 107, 55.0, 5.5, 1, 1, 10, 202404),
+    (7, 'Location A7', 108, 60.0, 6.0, 2, -1, 11, 202404),
+    (7, 'Location A7', 109, 65.0, 6.5, 3, 1, 12, 202404);
+	
+	
+	
+	
+	(
+
+select location_no,location_name,
+
+coalesce(location_no::varchar,'')||'_'||coalesce(location_name,'') as location_no_name,
+
+customers,Sales,Units,Visits from (SELECT
+
+location_no,location_name,
+
+count(distinct ucid ) as Customers,
+
+sum(item_tran_selling - item_discount_selling) as Sales,
+
+sum(item_tran_qty) as UNITS,
+
+(count(distinct case when tran_ind=1 then basket_id end)- count(distinct case when tran_ind=-1 then basket_id end))
+
+as Visits FROM sample_layeeq
+
+WHERE fin_year_month_no in (202404)
+
+AND UCID in (select distinct UCID FROM sample_layeeq
+
+             WHERE fin_year_month_no in (202404) AND location_no in (1)) group by 1,2)
+
+                                                where location_no not in (1)  order by customers desc limit 10);
+												
+	-------------------------------------------------------------
+												(
+select (select distinct location_no from sample_layeeq where location_no in (9)) as location_no1,
+(select distinct location_name from sample_layeeq where location_no in (9)) as location_name1, location_no,location_name,
+
+coalesce(location_no::varchar,'')||'_'||coalesce(location_name,'') as location_no_name,
+
+customers,Sales,Units,Visits from (SELECT
+
+location_no,location_name,
+
+count(distinct ucid ) as Customers,
+
+sum(item_tran_selling - item_discount_selling) as Sales,
+
+sum(item_tran_qty) as UNITS,
+
+(count(distinct case when tran_ind=1 then basket_id end)- count(distinct case when tran_ind=-1 then basket_id end))
+
+as Visits FROM sample_layeeq
+
+WHERE fin_year_month_no in (202404)
+
+AND UCID in (select distinct UCID FROM sample_layeeq
+
+             WHERE fin_year_month_no in (202404) AND location_no in (9)) group by 1,2)
+
+                                                where location_no not in (9)  order by customers desc limit 10);
+----------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+--create and populate the sample table
+  create table employee (
+  id int,
+  name varchar (20),
+  manager_id int
+  );
+  
+  insert into employee(id, name, manager_id)  values
+(100, 'Carlos', null),
+(101, 'John', 100),
+(102, 'Jorge', 101),
+(103, 'Kwaku', 101),
+(110, 'Liu', 101),
+(106, 'Mateo', 102),
+(110, 'Nikki', 103),
+(104, 'Paulo', 103),
+(105, 'Richard', 103),
+(120, 'Saanvi', 104),
+(200, 'Shirley', 104),
+(201, 'Sofía', 102),
+(205, 'Zhang', 104);
+  
+  location_no INT,
+    location_name VARCHAR(50),
+    ucid INT,
+    item_tran_selling DECIMAL(10, 2),
+    item_discount_selling DECIMAL(10, 2),
+    item_tran_qty INT,
+    tran_ind INT,
+    basket_id INT,
+    fin_year_month_no INT
+  
+  
+--run the recursive query
+  with recursive layeeq_org(location_no, location_name, ucid,item_tran_selling,item_discount_selling,item_tran_qty,
+  tran_ind,basket_id,fin_year_month_no, level) as
+( select location_no, location_name, ucid,item_tran_selling,item_discount_selling,item_tran_qty,
+  tran_ind,basket_id,fin_year_month_no, 1 as level
+  from sample_layeeq
+  --where name = 'John'
+  union all
+  select e.location_no, e.location_name, e.ucid,e.item_tran_selling,e.item_discount_selling,e.item_tran_qty,
+  e.tran_ind,e.basket_id,e.fin_year_month_no, level + 1 as next_level
+  from sample_layeeq e, layeeq_org j
+  where e.location_no = j.location_no and level < 1
+  )
+ select distinct location_no, location_name, ucid,item_tran_selling,item_discount_selling,item_tran_qty,
+  tran_ind,basket_id,fin_year_month_no,level from layeeq_org order by location_no limit 10;
+  
+  
+  WITH RECURSIVE LocationNumbers AS (
+    SELECT 1 AS location_no
+    UNION
+    SELECT location_no + 1
+    FROM LocationNumbers
+    WHERE location_no < 20 -- Adjust the upper limit as needed
+    LIMIT 100  -- Adjust this limit to avoid recursion limit
+)
+
+SELECT
+    ln.location_no,
+    customers,
+    Sales,
+    Units,
+    Visits
+FROM
+    (
+        SELECT
+            location_no,
+            COUNT(DISTINCT cust_no) AS Customers,
+            SUM(item_tran_selling - item_discount_selling) AS Sales,
+            SUM(item_tran_qty) AS Units,
+            (
+                COUNT(DISTINCT CASE WHEN tran_ind = 1 THEN basket_id END) -
+                COUNT(DISTINCT CASE WHEN tran_ind = -1 THEN basket_id END)
+            ) AS Visits
+        FROM
+            development.sample_table
+        WHERE
+            cust_no IN (
+                SELECT DISTINCT cust_no
+                FROM development.sample_table
+                WHERE location_no IN (SELECT location_no FROM LocationNumbers)
+            )
+        GROUP BY 1
+    ) AS ln
+WHERE
+    ln.location_no IS NOT NULL
+ORDER BY
+    customers DESC
+LIMIT 10;
+
+
+CREATE TABLE development.sample_table (
+    location_no INT,
+    cust_no INT,
+    item_tran_selling DECIMAL,
+    item_discount_selling DECIMAL,
+    item_tran_qty INT,
+    tran_ind INT,
+    basket_id INT
+);
+
+INSERT INTO development.sample_table VALUES
+(1, 101, 50.0, 5.0, 2, 1, 1),
+(1, 102, 30.0, 3.0, 1, 1, 1),
+(2, 103, 40.0, 4.0, 3, -1, 2),
+(2, 104, 20.0, 2.0, 2, 1, 2),
+(3, 105, 60.0, 6.0, 4, 1, 3);
+
+
+WITH RECURSIVE LocationNumbers AS (
+    SELECT 1 AS location_no
+    UNION
+    SELECT location_no + 1
+    FROM LocationNumbers
+    WHERE location_no < 4 -- Adjust the upper limit as needed
+    LIMIT 4  -- Adjust this limit to avoid recursion limit
+)
+
+SELECT
+    ln.location_no,
+    customers,
+    Sales,
+    Units,
+    Visits
+FROM
+    (
+        SELECT
+            location_no,
+            COUNT(DISTINCT cust_no) AS Customers,
+            SUM(item_tran_selling - item_discount_selling) AS Sales,
+            SUM(item_tran_qty) AS Units,
+            (
+                COUNT(DISTINCT CASE WHEN tran_ind = 1 THEN basket_id END) -
+                COUNT(DISTINCT CASE WHEN tran_ind = -1 THEN basket_id END)
+            ) AS Visits
+        FROM
+            development.sample_table
+        WHERE
+            cust_no IN (
+                SELECT DISTINCT cust_no
+                FROM development.sample_table
+                WHERE location_no IN (SELECT location_no FROM LocationNumbers)
+            )
+        GROUP BY 1
+    ) AS ln
+WHERE
+    ln.location_no IS NOT NULL
+ORDER BY
+    customers DESC
+LIMIT 10;
